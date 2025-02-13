852c93ed1b783a747e5f8be6de2882ba9a0e03a8
elodie-boweren <elodie.boweren@laplateforme.io>
2025-02-13 11:45:27 +0100

    feat: change selection to enumerate function, prepare second_menu subclass

--- menu.py
+++ menu.py
@@ -16,12 +16,13 @@ class Button:
         self.window.screen_menu.blit(self.image, (self.rect.x, self.rect.y))
 
 
-class Menu():
-    #first menus
+class Menu:
+    #first menu
     def __init__(self):
 
         self.window = Window()
         self.background = pygame.image.load("assets/pictures/menu.jpg")
+        self.selected_position = 1
         
         #Load button images:
         self.sign_in_img = self.window.create_text_image("Sign in", self.window.text_font_menu, self.window.BLACK)
@@ -30,8 +31,6 @@ class Menu():
         #Create Button objects
         self.sign_in_button = Button(100,100,self.sign_in_img, self.window)
         self.new_player_button = Button(530,100,self.new_player_img, self.window)
-
-        self.selected_position = 1 
 
     def draw_background(self):
         """function to draw background"""
@@ -42,24 +41,60 @@ class Menu():
         self.new_player_button.draw_button()
 
     #draw a rectangle around the currently selected button
-    def draw_selected_button(self):
-        if self.selected_position == 1:
-            button_rect = self.sign_in_button.rect
-        elif self.selected_position == 2:
-            button_rect = self.new_player_button.rect
+    def select_menu_button(self):
+        buttons = (self.sign_in_button, self.new_player_button)
+        for position, button in enumerate(buttons, 1):
+            #check if button is selected
+            if position == self.selected_position:  
+                #draw the rectangle around it
+                pygame.draw.rect(self.window.screen, self.window.GREY, button.rect, 3)
+      
 
-        pygame.draw.rect(self.window.screen, self.window.GREY, button_rect, 3)
+# # class Second_menu(Menu):
+# #     def __init__(self):
+# #         super().__init__(self)
+
+# #          #Load button images:
+# #         self.new_game_img = self.window.create_text_image("New game", self.window.text_font_menu, self.window.BLACK)
+# #         self.pokedex_img = self.window.create_text_image("Pokedex", self.window.text_font_menu, self.window.BLACK)
+# #         self.resume_game_img = self.window.create_text_image("Resume game", self.window.text_font_menu, self.window.BLACK)
+#         # self.add_pokemon_img = self.window.create_text_image("Add pokemon", self.window.text_font_menu, self.window.BLACK)
+
+# #         #Create Button objects
+# #         self.new_game_button = Button(100,100,self.new_game_img, self.window)
+# #         self.pokedex_button = Button(530,100,self.pokedex_img, self.window)
+# #         self.resume_game_button = Button(530,100,self.resume_game_img, self.window)
+#         # self.add_pokemon_button = Button(530,100,self.add_pokemon_img, self.window)
+
+# #     def draw_buttons(self):
+#         self.resume_game_button.draw_button()
+# #         self.pokedex_button.draw_button()
+# #         
+#         self.add_pokemon_button.draw_button()
+#         self.new_game_button.draw_button()
+
+#  #draw a rectangle around the currently selected button
+#     def select_menu_button(self):
+#         buttons = (self.resume_game_button, self.pokedex_button, self.add_pokemon_button, self.new_game_button)
+#         for position, button in enumerate(buttons, 1):
+#             #check if button is selected
+#             if position == self.selected_position:  
+#                 #draw the rectangle around it
+#                 pygame.draw.rect(self.window.screen, self.window.GREY, button.rect, 3)
+
 
 
 pygame.init()
+
 run = True
+
 menu = Menu()
-position = 1
+
 while run:
     
     menu.draw_background()
     menu.draw_buttons()
-    menu.draw_selected_button()
+    menu.select_menu_button()
 
     #events
     for event in pygame.event.get():
@@ -70,11 +105,11 @@ while run:
             if event.key == K_RIGHT:
                menu.selected_position = 2 if menu.selected_position == 1 else 1
             
-            elif event.key == K_RETURN:
+            # elif event.key == K_RETURN:
                 # if menu.selected_position == 1:
                 #     sign_in()
-                if menu.selected_button == 2:
-                    game()
+                # if menu.selected_button == 2:
+                #     game()
                     
 
     pygame.display.update()
