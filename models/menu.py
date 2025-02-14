13419b83ba33269eddb53dbffd9508c7d62d15a2
elodie-boweren <elodie.boweren@laplateforme.io>
2025-02-14 12:45:48 +0100

    feat: modify files to enable main to work on menus

--- models/menu.py
+++ models/menu.py
@@ -44,43 +44,6 @@ class Menu:
                 self.selected_position = (self.selected_position % 2) +1 
         pygame.display.update()
                 
-    def menu_loop():
-        pygame.init()
-        run = True
-        menu = Menu()
-        second_menu=Second_menu()
-        pokedex_menu = Pokedex_menu()
-        current_menu = menu
-        while run:
-            #set the scene
-            current_menu.draw_background()
-            current_menu.draw_buttons()
-            current_menu.select_menu_button()
-
-            #handle events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                #handle events based on the type of menu
-                current_menu.handle_events(event)
-
-                if event.type == KEYDOWN and event.key == K_RETURN:
-                    if current_menu == menu:
-                        current_menu = second_menu
-                    else:
-                        current_menu = menu
-                    
-                    # elif event.key == K_RETURN:
-                        # if menu.selected_position == 1:
-                        #     sign_in()
-                        # if menu.selected_button == 2:
-                        #     game()
-                            
-
-            pygame.display.update()
-
-
-        pygame.quit()
 
 
 def Create_player():
