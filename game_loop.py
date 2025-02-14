2c21ce099436fa22b53ed0cecf77b3b7f60b2f17
elodie-boweren <elodie.boweren@laplateforme.io>
2025-02-14 11:13:27 +0100

    feat: menu and second menu connected, selection method created

--- game_loop.py
+++ game_loop.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame.locals import *
-
 from fight import Fight
 from pokemon import Pokemon
 from trainer import Trainer
@@ -15,28 +14,51 @@ fps = 60
 fight = Fight()
 pokemon = Pokemon()
 trainer = Trainer()
+menu = Menu()
+
+game_state = "state"
 position = 1
+
 run = True
-# draw background
-fight.draw_background_fight()
 
-# draw pokemon
-trainer.draw_pokemon_opponent_hp()
-trainer.draw_pokemons()
 
-# draw bottom panel
-fight.draw_panel()
-trainer.draw_panel_button(position) 
-
-def game():
+def main_loop():
     while run:
 
-        clock.tick(fps)
+        # #different states of the program
+        # if game_state == "menu":
+        #     menu()
+        # elif game_state == "second_menu":
+        #     second_menu()
+        # elif game_state == "new player":
+        #     new_player()
+        # elif game_state == "sign in":
+        #     sign_in()
+        # elif game_state == "resume game":
+        #     play()
+        # elif game_state == "add pokemon":
+        #     add_pokemon()
+        # elif game_state == "pokedex":
+        #     display_pokedex()
+        # elif game_state == "new game":
+        #     play_new_game()
 
-
+        #handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+
+        # draw background
+        fight.draw_background_fight()
+
+        # draw pokemon
+        trainer.draw_pokemon_opponent_hp()
+        trainer.draw_pokemons()
+
+        # draw bottom panel
+        fight.draw_panel()
+        trainer.draw_panel_button(position) 
 
         if event.type == KEYDOWN:
             if event.key == K_DOWN:
@@ -53,6 +75,6 @@ def game():
                     trainer.trainer_attack()
 
         pygame.display.update()
-
+        clock.tick(fps)
 
     pygame.quit()
