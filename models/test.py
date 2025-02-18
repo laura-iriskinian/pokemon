9c89d4b1f5bfa3e210bf3b06f2fa14d58a245026
elodie-boweren <elodie.boweren@laplateforme.io>
2025-02-18 12:03:35 +0100

    del: deleted test.py

--- models/test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("models/pokedex.json", "r", encoding = "utf-8") as file:
-    pokedex = json.load(file)
-player_name = ""
-for data in pokedex:
-            for player in data["players"]:
-                print(type(data))
-                    
